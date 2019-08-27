ed5a80d9d6100520ef4605af3a8fc81490c4afa3
saurabhhtiwarii@gmail.com <saurabhhtiwarii@gmail.com>
2019-08-27 19:29:31 +0530

    Added new call_to_action component

---
+++ core/src/main/java/com/themecleanflex/models/Call_to_actionModel.java
@@ -0,0 +1,93 @@
+package com.themecleanflex.models;
+
+import com.peregrine.nodetypes.models.AbstractComponent;
+import com.peregrine.nodetypes.models.IComponent;
+import com.peregrine.nodetypes.models.Container;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/*
+    //GEN[:DATA
+    {
+  "definitions": {
+    "Call_to_action": {
+      "type": "object",
+      "x-type": "component",
+      "properties": {
+        "text": {
+          "type": "string",
+          "x-source": "inject",
+          "x-form-type": "text"
+        },
+        "url": {
+          "type": "string",
+          "x-source": "inject",
+          "x-form-label": "URL",
+          "x-form-type": "url"
+        }
+      }
+    }
+  },
+  "name": "Call_to_action",
+  "componentPath": "themecleanflex/components/call_to_action",
+  "package": "com.themecleanflex.models",
+  "modelName": "Call_to_action",
+  "classNameParent": "AbstractComponent"
+}
+//GEN]
+*/
+
+//GEN[:DEF
+@Model(
+        adaptables = Resource.class,
+        resourceType = "themecleanflex/components/call_to_action",
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        adapters = IComponent.class
+)
+@Exporter(
+        name = "jackson",
+        extensions = "json"
+)
+
+//GEN]
+public class Call_to_actionModel extends AbstractComponent {
+
+    public Call_to_actionModel(Resource r) { super(r); }
+
+    //GEN[:INJECT
+    	/* {"type":"string","x-source":"inject","x-form-type":"text"} */
+	@Inject
+	private String text;
+
+	/* {"type":"string","x-source":"inject","x-form-label":"URL","x-form-type":"url"} */
+	@Inject
+	private String url;
+
+
+//GEN]
+
+    //GEN[:GETTERS
+    	/* {"type":"string","x-source":"inject","x-form-type":"text"} */
+	public String getText() {
+		return text;
+	}
+
+	/* {"type":"string","x-source":"inject","x-form-label":"URL","x-form-type":"url"} */
+	public String getUrl() {
+		return url;
+	}
+
+
+//GEN]
+
+    //GEN[:CUSTOMGETTERS
+    //GEN]
+
+}
---
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/call_to_action/template.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row" style="height:60px;" v-bind:data-per-path="model.path">
+    <div class="col-md-8">
+      <p style="margin-left: 5px; margin-top: 10px;">{{model.text}}</p>
+    </div>
+    <div class="col-md-4">
+      <a v-bind:href="model.url">
+        <button class="btn btn-success" style="margin-top: 10px;">Subscribe</button>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
+
