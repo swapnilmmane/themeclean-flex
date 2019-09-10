3c01cc7924ec17d64a4938e29b06f097a4268724
saurabhhtiwarii@gmail.com <saurabhhtiwarii@gmail.com>
2019-09-10 16:04:57 +0530

    Added new Banner component in themeclean-flex.

---
+++ fragments/banner/template.vue
@@ -0,0 +1,18 @@
+<template>
+  <themecleanflex-components-block v-bind:model="model">
+    <banner class="w-full">
+      <div class="relative overflow-hidden">
+        <img class="w-full h-full object-center" v-bind:src="model.image">
+        <h2 class="absolute top-0 p-4 text-white text-xl w-full" v-html="model.title"></h2>
+        <p class="absolute bottom-0 p-4 text-white text-xl w-full" v-html="model.text"></p>
+      </div>
+    </banner>
+  </themecleanflex-components-block>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
+
