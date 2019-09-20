6b372461286bc222f90a2cfc04fff77cb4fa9c4c
saurabhhtiwarii@gmail.com <saurabhhtiwarii@gmail.com>
2019-09-20 15:19:42 +0530

    Added External Block component in themeclean-flex theme.

---
+++ fragments/externalblock/template.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-bind:data-per-path="model.path">
+    <a v-bind:href="model.url">
+      <h2>{{model.title}}</h2>
+    </a>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
+
